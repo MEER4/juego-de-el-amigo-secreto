d28afe1def406806694e57089093a20ac36e61c3
MEER4 <mario.estrella.ing.software@gmail.com>
2025-07-25 15:39:22 -0400

    se agregaron comentarios, y leves cambio para mejor  entendimiento

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -4,47 +4,55 @@
  
 const listaDeAmigos = [];
 
-
+// creo una funcion para agregar los amigos en el array
 function agregarAmigo(){
     const amigos = document.getElementById("amigo");
     let valorInput = amigos.value;
 
+    //verifico  si el input esta vacio con .trim() y agrego una alerta indicandole al jugador que ingrese un nombre, cuando este vacio 
     if (valorInput.trim() === "") {
         alert("Por favor, inserte un nombre.");
         return; // Salir de la función si el input está vacío
-    } else {
+    } else { //agrego los nombres con .push
         listaDeAmigos.push(valorInput.trim());
-        console.log(listaDeAmigos)
+        console.log(listaDeAmigos)// uso console.log para verificar si se estan guardando los nombres
         amigos.value = '';
         amigos.focus();
-        listarAmigos()
+        listarAmigos() 
         return;
         
     }
 }
 
+// creo la funcion que listara los nombres agregados
 function listarAmigos() {
-    const listadoDeAmigos = document.getElementById("listaAmigos");
-    listadoDeAmigos.innerHTML = "";
+    const listadoDeAmigos = document.getElementById("listaAmigos"); 
+    listadoDeAmigos.innerHTML = "";// verifico si la lista esta vacia
 
-    listaDeAmigos.forEach(listaDeAmigos => {
-        const nuevoElemento = document.createElement("li");
-        nuevoElemento.textContent = listaDeAmigos;
-        listadoDeAmigos.appendChild(nuevoElemento);
+    //busco los nombres en las lista con un .forEach
+    listaDeAmigos.forEach(amigos => {
+        const nuevoElemento = document.createElement("li");// creo el lemento li(lista) dentro de la lista ol
+        nuevoElemento.textContent = amigos;// asigno a cada nuevo elemento li el valor amigo= nombres en el array lo crea
+        listadoDeAmigos.appendChild(nuevoElemento); // y con la variable asigno el texto a la lista
 
     } )
     
   
 }
 
+// creo la funcion para sortear
 function sortearAmigo(){
-    const resultado = document.getElementById("resultado")
+    const resultado = document.getElementById("resultado")// tomo el id de el boton para activar la funcion
     
+    // verifico si hay amigos en la lista
     if (listaDeAmigos.length === 0){
-        resultado.innerHTML = "No hay amigos en la lista para sortear"
-        
+        resultado.innerHTML = "No hay amigos en la lista para sortear" // envio el mensaje de tener la lista vacia
+
     }
 
+    // creo la variable que genera el amigo aleatorio.
+    // uso con las funciones math(.floor y .random) obtengo un numero aleatorio esto lo multiplico por el numero de nombres guardados  con .length
+    // con la variable de amigosorteado llamo a lista y selecciono indice con el numero generado aleatorio
     const generarAmigoAleatorio = Math.floor(Math.random()* listaDeAmigos.length);
 
     const amigoSorteado = listaDeAmigos[generarAmigoAleatorio];
